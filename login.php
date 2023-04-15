49d1223d3d74a6cd6f9e5fb811f0385023fe176f
Josh Hoefs <jehoefs@mtu.edu>
2023-04-15 13:07:34 -0400

    added line break

--- login.php
+++ login.php
@@ -37,6 +37,7 @@
             <input type="text" id="username" name="username"><br>
             <label for="password">Password:</label><br>
             <input type="text" id="password" name="password"><br>
+            <br>
             <input type="submit" value="Student Login" name="stulogin">
             <input type="submit" value="Instructor Login" name="instlogin">
 
