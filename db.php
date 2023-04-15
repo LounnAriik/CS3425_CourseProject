0a0a63cd21c4944736d777c513e42afa86c2cabe
Josh Hoefs <jehoefs@mtu.edu>
2023-04-15 13:01:18 -0400

    updated authenitcate methods

--- db.php
+++ db.php
@@ -6,14 +6,31 @@ function connectDB()
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbh;
 }
-//return number of rows matching the given user and passwd.
-function authenticate($user, $password, $selectedTable) {
+
+//return number of rows matching the given user and passwd for student login.
+function authenticateStudent($user, $password) {
     try {
         $dbh = connectDB();
-        $statement = $dbh->prepare("SELECT count(*) FROM project_:selectedTable ".
+        $statement = $dbh->prepare("SELECT count(*) FROM project_student ".
         "where username = :username and password = sha2(:password,256) ");
-        $statement->bindParam(":selectedTable", $selectedTable);
         $statement->bindParam(":username", $user);
+        $statement->bindParam(":password", $password);
+        $result = $statement->execute();
+        $row=$statement->fetch();
+        $dbh=null;
+        return $row[0];
+    }catch (PDOException $e) {
+        print "Error!" . $e->getMessage() . "<br/>";
+        die();
+    }
+}
+
+//return number of rows matching the given user and passwd for instructor login.
+function authenticateStudent($user, $password) {
+    try {
+        $dbh = connectDB();
+        $statement = $dbh->prepare("SELECT count(*) FROM project_instructor ".
+        "where username = :username and password = sha2(:password,256) ");
         $statement->bindParam(":password", $password);
         $result = $statement->execute();
         $row=$statement->fetch();
