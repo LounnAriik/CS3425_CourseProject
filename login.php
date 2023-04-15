f1708669204c0f7e96792b268da7c420ba421034
LounnAriik <aphamach@mtu.edu>
2023-04-15 11:42:43 -0400

    Add login

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+?>
+<html>
+    <body>
+        <form method=post action=login.php>
+            
+            <?php
+            require "db.php";
+                if (isset($_POST["logout"])) {
+                    session_destroy();
+                    header("LOCATION: login.php");
+                    exit();
+                    }
+                if(isset($_POST["login"])) {
+                    
+                    if(authenticate($_POST["username"], $_POST["password"])==1){
+                        $_SESSION["username"]=$_POST["username"];
+                        header("LOCATION:main.php");
+                        return;
+                    }else{
+                        echo '<p style="color:red"> incorrect username and password</p>';
+                    }
+                }
+               
+            ?>
+            <label for="username">Username:</label><br>
+            <input type="text" id="username" name="username"><br>
+            <label for="password">Password:</label><br>
+            <input type="text" id="password" name="password"><br>
+            <input type="submit" value="Submit" name="login">
+        </form>
+    </body>
+</html>
