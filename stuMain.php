2e089ea7808bfe7d48faae26b1c3cf5098a6a0c3
LounnAriik <aphamach@mtu.edu>
2023-04-16 12:31:16 -0400

    Student main page table

--- stuMain.php
+++ stuMain.php
@@ -19,7 +19,30 @@
         <?php
             }
         ?>
-        
+        <?php
+            require "db.php";
+            $classes = getCoursesTaking($_SESSION["username"]);
+        ?>
+            <table>
+                <tr>
+                    <th>C_ID</th>
+                    <th>Title</th>
+                    <th>Credit</th>
+                    <th>Instructor</th>
+                    <th>Survey Time</th>
+                </tr>
+        <?php
+            foreach($classes as $row){
+                echo "<tr>";
+                echo "<td>" . $row[0] . "</td>";
+                echo "<td>" . $row[1] . "</td>";
+                echo "<td>" . $row[2] . "</td>";
+                echo "<td>" . $row[3] . "</td>";
+                echo "<td>" . $row[4] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        ?>
         
     </body>
 </html>
